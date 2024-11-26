82ad2794e22743e5850726808e13b5e017404bc7
Raunak Dey <91902527+IamRaunakDey@users.noreply.github.com>
2024-11-27 00:01:46 +0530

    Update StudentRepository.java

--- Library-Management-System-main/src/main/java/com/StudentLibrary/Studentlibrary/Repositories/StudentRepository.java
+++ Library-Management-System-main/src/main/java/com/StudentLibrary/Studentlibrary/Repositories/StudentRepository.java
@@ -11,8 +11,7 @@ import java.util.List;
 @Transactional
 public interface StudentRepository extends JpaRepository<Student,Integer> {
 
-    //JPQL-->Java persistence Query language-->(Objects and Attributes)
-    //Native sql query-->(columns and tables)
+  
 
 
     @Modifying
@@ -32,20 +31,12 @@ public interface StudentRepository extends JpaRepository<Student,Integer> {
             "s.country=:#{#student.country} where s.id=:#{#student.id}")
     int updateStudentDetails(@Param("student") Student student);
 
-    //find student by given name
-    //Terminal---> Select * from student where email =email
-    //1. JPQL--Dealing with java objects
-    //Student(exact class name) class has the variable name as emailId so b.emailId
-    // :mail has to passed in the argument of the function exact variable name as in the args
+
 
     @Query("select b from Student b where b.emailId=: mail")
     List<Student> find_by_mail(String mail);
 
-    //Native sql query -- dealing with sql tables
-    //SQL table formed with name student not Student
-    //if the variable name is emailId then parameter used in query is email_id
-    //Hibernate converts camel case to _ separated names
-    //Think of it as a sql table
+    
 
 
     @Query(value = "select * from student s where s.email_id=:mail",nativeQuery = true)
