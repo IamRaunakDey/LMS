089fd539765ff467700bb237ee88d72d69dc0cec
Raunak Dey <91902527+IamRaunakDey@users.noreply.github.com>
2024-11-27 00:02:07 +0530

    Update CardRepository.java

--- Library-Management-System-main/src/main/java/com/StudentLibrary/Studentlibrary/Repositories/CardRepository.java
+++ Library-Management-System-main/src/main/java/com/StudentLibrary/Studentlibrary/Repositories/CardRepository.java
@@ -15,7 +15,5 @@ public interface CardRepository extends JpaRepository<Card,Integer> {
     @Query(value = "update card c set c.card_status=:new_card_status where c.id in(select card_id from student s where s.id=:student_id)",nativeQuery = true)
     void deactivateCard(@Param("student_id") int student_id,@Param("new_card_status") String new_card_status);
 
-//    @Modifying
-//    @Query("update Card c set c.books=:#{#new_card.books} where c.id=:#{#new_card.id}")
-//    int updateCard(@Param("new_card") Card card);
+
 }
